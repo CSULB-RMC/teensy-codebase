442cb598213da18934137118d2829a27bb8aec98
Anthony Le <aznthonyle45@gmail.com>
2022-03-04 13:39:18 -0800

    Removed Spark_Conversion file. Modified test.cpp with the implementation of directional functions (forward, backward, and netural). Tests of PWM signal verified with Oscilloscope.

--- Drivetrain/test.cpp
+++ Drivetrain/test.cpp
@@ -1,32 +1,80 @@
 #include <Arduino.h>
 
-#define LED_60Hz 2
-#define LED_Base 1
+#define DRIVETRAIN_PIN 2
+#define PWM_FREQ 100
 
-int pwm_signal = 0;
+#define FORWARD_MIN   39
+#define FORWARD_MAX   51
+#define NEUTRAL       38
+#define BACKWARD_MIN  25
+#define BACKWARD_MAX  37
+
+// Variable to write to Teensy
+int pulse_duration = 0;
+
+// This is drivetrain proportional forward
+void drivetrain_forward(int speed){
+
+  // Map out the value of the pulse
+  pulse_duration = map(speed, 0, 100, FORWARD_MIN, FORWARD_MAX);
+  
+  // Set the duty cycle
+  analogWrite(DRIVETRAIN_PIN, pulse_duration);
+
+}
+
+// This is drivetrain proportional backwards
+void drivetrain_backwards(int speed){
+
+  // Map out the value of the pulse
+  pulse_duration = map(speed, 0, 100, BACKWARD_MIN, BACKWARD_MAX);
+  
+  // Set the duty cycle
+  analogWrite(DRIVETRAIN_PIN, pulse_duration);
+}
+
+// This is drivetrain with a setting for direction and speed
+// direction = 0 (Back), direction = 1 (forward)
+void drivetrain_move(int direction, int speed){
+
+  // Map out the value of the pulse
+  if (direction){
+    pulse_duration = map(speed, 0, 100, FORWARD_MIN, FORWARD_MAX);
+  }
+  else{
+    pulse_duration = map(speed, 0, 100, BACKWARD_MIN, BACKWARD_MAX);
+  }
+  // Set the duty cycle
+  analogWrite(DRIVETRAIN_PIN, pulse_duration);
+}
+
+// Drivetrain Neutral mode
+void drivetrain_neutral(){
+
+  analogWrite(DRIVETRAIN_PIN, NEUTRAL);
+}
 
 void setup() {
   
   // Set the pins to OUTPUT
-    pinMode(LED_60Hz, OUTPUT);
-    pinMode(LED_Base, OUTPUT);
+    pinMode(DRIVETRAIN_PIN, OUTPUT);
 
-  // Set pin2 LED_60Hz to 60 Hz frequency
-    analogWriteFrequency(LED_60Hz, 60);
+  // Set DRIVETRAIN_PIN to the PWM frequency
+    analogWriteFrequency(DRIVETRAIN_PIN, PWM_FREQ);
+
+  // Loop through speeds for backwards and forwards
+  for(int dir = 0; dir < 2; dir++){
+    for(int spd = 0; spd <= 100; spd++){
+      
+      // Set the speed and direction 
+      drivetrain_move(dir, spd);
+      
+      // Wait for 500ms 
+      delay(500);
+    }
+  }
 }
 
 void loop() {    
-  // Will start with a duty cycle of 0 and ramp up to 100
-    for(int i = 0; i <= 100; i++){
 
-        // Maps the corresponding signal to the resolution of the pin
-        pwm_signal = map(i, 0, 100, 0, 255);
-
-        // Output the translated duty cycle
-        analogWrite(LED_60Hz, pwm_signal);
-        analogWrite(LED_Base, pwm_signal);
-
-        // Wait for 1/2 second
-        delay(500);
-    }
 }
