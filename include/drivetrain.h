3437d285f86e529df30f6fe9c323e29a6c127130
Anthony Le <aznthonyle45@gmail.com>
2022-03-09 17:50:50 -0800

    Within header, figured out solution for initializing SparkMax objects. Furthermore created movement functions for Drivetrain, and added default values for Drivetrain constructor. Created following implementation file for Drivetrain.

--- include/drivetrain.h
+++ include/drivetrain.h
@@ -1,11 +1,22 @@
 #ifndef DRIVETRAIN_H
 #define DRIVETRAIN_H
+#include "sparkMax.h"
 
-class Drivetrain{
+class Drivetrain{    
+
+    private:
+        // Initialize SparkMax objects
+        SparkMax left_motor;
+        SparkMax right_motor;
+    
     public:
         // Constructor 
-        Drivetrain(int, int);
-};
+        Drivetrain(int=0, int=0);
 
+        // Movement functions for the Drivetrain
+        void move_forward(int=0);
+        void move_backward(int=0);
+        void move_neutral(int=0);
+};
 
 #endif
--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -1,9 +1,34 @@
 #include "sparkMax.h"
 #include "drivetrain.h"
 
-Drivetrain::Drivetrain(int left_motor, int right_motor){
+Drivetrain::Drivetrain(int left_pin, int right_pin){
 
-    // Call the sparkMax motor constructors and pass in the pins
-    SparkMax Left_Motor(left_motor);
-    SparkMax Right_Motor(right_motor);
+    // Set the objects to the corresponding pins
+    left_motor.set_motor_pin(left_pin);
+    right_motor.set_motor_pin(right_pin);
+
+}
+
+void Drivetrain::move_forward(int speed){
+
+    // Move the motors forwards
+    left_motor.move(1,speed);
+    right_motor.move(1, speed);
+
+}
+
+void Drivetrain::move_backward(int speed){
+
+    // Move the motors backwards
+    left_motor.move(0,speed);
+    right_motor.move(0, speed);
+
+}
+
+void Drivetrain::move_neutral(int speed){
+
+    // Make the motors stop
+    left_motor.stop();
+    right_motor.stop();
+
 }
