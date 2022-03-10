429a119d733f152bf3e09201a94c40878c9e7f59
Eric Wang <ericwang3.1415@gmail.com>
2022-03-10 00:16:02 -0800

    Added linear actuator methods

--- include/regcon.h
+++ include/regcon.h
@@ -6,6 +6,11 @@
 
 class RegCon {
 private:
+    int lin_left1;
+    int lin_left2;
+    int lin_right1;
+    int lin_right2;
+
     // Spark objects for bag motors for conveyor
     Spark left_spark;
     Spark right_spark;
@@ -13,17 +18,24 @@ private:
 public:
     // Constructor
     // Parameters are for spark pins, not thinking bout liner actuators for now
-    RegCon(int, int); 
+    RegCon(int, int, int, int, int, int); 
 
     // Linear actuator methods, uses two linear actuators
     void linear_up();
     void linear_down();
     void linear_stop();
 
+    // Setter methods for linear actuator
+    void set_lin_pins(int, int, int, int);
+    void set_lin_inputs(int, int, int, int);
+
     // Conveyor methods, uses two bag motors
     void conveyor_forward();
     void conveyor_backward();
     void conveyor_stop();
+
+    // Setter methods for conveyor sparks
+    void set_spark_pins(int, int);
 };
 
 #endif
--- src/regcon.cpp
+++ src/regcon.cpp
@@ -1,24 +1,41 @@
+#include <Arduino.h>
 #include <Servo.h>
 #include "spark.h"
 #include "regcon.h"
 
 // Set the objects to the corresponding pins
-RegCon::RegCon(int pin1, int pin2) {
-    left_spark.set_motor_pin(pin1);
-    right_spark.set_motor_pin(pin2);
+RegCon::RegCon(int lin_left_pin1, int lin_left_pin2, int lin_right_pin1, int lin_right_pin2, int spark_left_pin, int spark_right_pin) {
+    set_lin_pins(lin_left_pin1, lin_left_pin2, lin_right_pin1, lin_right_pin2);
+    set_spark_pins(spark_left_pin, spark_right_pin);
 }
 
-// TODO
+// Sets linear actuator inputs based on table in bucket ladder documentation
 void RegCon::linear_up() {
-
+    set_lin_inputs(1, 0, 1, 0);
 }
 
 void RegCon::linear_down() {
-
+    set_lin_inputs(0, 1, 0, 1);
 }
     
 void RegCon::linear_stop() {
+    set_lin_inputs(1, 1, 1, 1);
+}
 
+// Sets linear actuator pins
+void RegCon::set_lin_pins(int l_pin1, int l_pin2, int r_pin1, int r_pin2) {
+    pinMode(l_pin1, OUTPUT);
+    pinMode(l_pin2, OUTPUT);
+    pinMode(r_pin1, OUTPUT);
+    pinMode(r_pin2, OUTPUT);
+}
+
+// Sets linear actuator inputs
+void RegCon::set_lin_inputs(int l_input1, int l_input2, int r_input1, int r_input2) {
+    digitalWrite(lin_left1, l_input1);
+    digitalWrite(lin_left2, l_input2);
+    digitalWrite(lin_right1, r_input1);
+    digitalWrite(lin_right2, r_input2);
 }
 
 // Conveyor belt moves forward/backward/stops by calling the equivalent method on both sparks
@@ -35,4 +52,10 @@ void RegCon::conveyor_backward() {
 void RegCon::conveyor_stop() {
     left_spark.spark_neutral();
     right_spark.spark_neutral();
+}
+
+// Sets pins for sparks
+void RegCon::set_spark_pins(int l_pin, int r_pin) {
+    left_spark.set_motor_pin(l_pin);
+    right_spark.set_motor_pin(r_pin);
 }
