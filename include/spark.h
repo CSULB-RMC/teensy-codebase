2ab0722004653d81f5d7d4436a93020a65ffd348
Anthony Le <aznthonyle45@gmail.com>
2022-03-11 16:46:41 -0800

    Reworked method visibility to be similar to drivetrain and linears. Only move, stop, and setting the pins are the only public methods a user can utilize. Furthermore, added variables for reference in the future for autonomy.

--- include/spark.h
+++ include/spark.h
@@ -3,21 +3,36 @@
 
 #include <Servo.h>
 
-// Motor controller for bag motor
+// Motor controller for bag motor (controls the sifter conveyers)
 class Spark {
 
 private:
+    // Variables for reference
+    int spark_pin,
+        spark_dir;
+
+    // Speed constants
+    const int FORWARD = 180,
+              BACKWARD = 0,
+              NEUTRAL = 90;
+
     // Built in servo library
     Servo servo;
-
-public:
-    // Constructor, parameter for pin number
-    Spark(int=0);
 
     // Motor methods
     void spark_forward();
     void spark_backward();
     void spark_neutral();
+
+public:
+    // Constructor, parameter for pin number
+    Spark(int=0);
+
+    // Function for moving 
+    void move(int);
+    
+    // Method for stopping the motors
+    void stop();
 
     // Setter for motor pin number
     void set_motor_pin(int);
--- src/spark.cpp
+++ src/spark.cpp
@@ -10,19 +10,49 @@ Spark::Spark(int pin_num){
     spark_neutral();
 }
 
-// Goes from 0 to 180, 90 is neutral
+// Goes from 0 to 180, 90 is neutral, 0 is backwards, and 180 is forwards
 void Spark::spark_forward() {
-    servo.write(180);
+    spark_dir = FORWARD;
+    servo.write(spark_dir);
 }
 
 void Spark::spark_backward() {
-    servo.write(0);
+    spark_dir = BACKWARD;
+    servo.write(spark_dir);
 }
 
 void Spark::spark_neutral() {
-    servo.write(90);
+    spark_dir = NEUTRAL;
+    servo.write(spark_dir);
 }
 
+// This method attaches the motor pin to the bag motor
 void Spark::set_motor_pin(int pin_num) {
+    
+    // Record the pin location
+    spark_pin = pin_num;
+
+    // Attach the pin
     servo.attach(pin_num);
+}
+
+// Backward = 0, Forwards = 1
+void Spark::move(int direction){
+
+    // If direction is 1...
+    if(direction){
+
+        spark_forward();
+    }
+
+    else{
+
+        spark_backward();
+    }
+}
+
+// Stops the sparks
+void Spark::stop(){
+
+    spark_neutral();
 }
