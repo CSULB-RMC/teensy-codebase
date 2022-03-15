3efa70eb6bbeaccd2293f4d0ebc04ee38e5ae945
Anthony Le <aznthonyle45@gmail.com>
2022-03-15 16:52:04 -0700

    Added object instantiation for motors and lin. act.

--- include/bucketLad.h
+++ include/bucketLad.h
@@ -1,12 +1,19 @@
 #ifndef BUCKETLAD_H
 #define BUCKETLAD_H
 
+#include "spark.h"
+#include "DRV8871.h"
+
 class BucketLadder{
 
     private:
-        
+        // Linear actuator class objects
+        DRV8871 left_linear;
+        DRV8871 right_linear;
 
-
+        // Motor instantiation
+        Spark mini_CIM;
+        Spark bag1, bag2;
 
     public:
 
