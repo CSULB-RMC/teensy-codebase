67f91ca8bee1ddf604cd2e77bf5ad4eebfd305c6
Anthony Le <aznthonyle45@gmail.com>
2022-03-23 17:24:24 -0700

    Fully tested and implemented 'theoretical' potentiometer value conversion for test.

--- src/Pot_Test.cpp
+++ src/Pot_Test.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define POT_PORT 14
+#define ONBOARD_LED 13
 
 int percent_out(int);
 int metric_out(int);
@@ -9,10 +10,12 @@ void setup(){
 
     // Set pin 14 to inpit
     pinMode(POT_PORT, INPUT);
+    pinMode(ONBOARD_LED, OUTPUT);
     Serial.begin(9600);
 }
 
-int pot_value = 0, 
+int pot_value = 0,
+    prev_pot_value = 0,
     percent_val = 0,
     metric_val = 0;
 
@@ -25,16 +28,24 @@ void loop(){
     percent_val = percent_out(pot_value);
     metric_val = metric_out(pot_value);
 
-    // Output
-    Serial.print(pot_value);
-    Serial.print("");
-    Serial.print(percent_val);
-    Serial.print("");
-    Serial.print(metric_val);
-    Serial.print("");
+    // Output to Serial and LED
+    analogWrite(ONBOARD_LED, pot_value/4);
 
-    // Wait 1 second before reading next value
-    delay(1000);
+    // Only output value when there is a change
+    if(prev_pot_value != pot_value){
+        Serial.print(pot_value);
+        Serial.print('\n');
+        Serial.print(percent_val);
+        Serial.print('\n');
+        Serial.print(metric_val);
+        Serial.print('\n');
+    }
+    
+    // Set the previous state
+    prev_pot_value = pot_value;
+
+    // Wait 100 ms before reading next value
+    delay(100);
 }
 
 int percent_out(int pot_val){
