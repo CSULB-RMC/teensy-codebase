e3fe1ba9b43b0750aa8efeab2ea43eeea94735e4
fenteale <mangmasta@gmail.com>
2022-04-19 23:53:10 -0700

    Fixed some probs and added test led

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/int8.h>
-
+#include <std_msgs/msg/empty.h>
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printFunc(#fn " Error Code: "); printFuncln(temp_rc); return -1;}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printFunc(#fn " Error Code: "); printFuncln(temp_rc);}}
@@ -20,6 +20,10 @@ rcl_node_t node;
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
+
+void test_LED_go(const void *msgin) {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
 
 void dt_left_callback(const void *msgin) {
     const std_msgs__msg__Int8 * speed = (const std_msgs__msg__Int8 *)msgin;
@@ -96,19 +100,23 @@ void setup() {
   RCSOFTCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   printFuncln("rclc_executor_init Success.");
 
+  pinMode(LED_BUILTIN, OUTPUT);
   dt = new Drivetrain(8, 9); //TODO: replace these with the actual pins, this is just a test
   //BucketLadder bl(2, 3, 4, 5, 6, 7, 8); //TODO: replace these with the actual pins
   //RegCon rc(9, 10, 11, 12, 13, 14); //TODO: replace with actual pins
   rcl_subscription_t dt_left_sub;
   rcl_subscription_t dt_right_sub;
+  rcl_subscription_t testLED_sub;
 
   std_msgs__msg__Int8 msg;
 
   RCSOFTCHECK(rclc_subscription_init_default(&dt_left_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8), "dt_left"));
   RCSOFTCHECK(rclc_subscription_init_default(&dt_right_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8), "dt_right"));
+  RCSOFTCHECK(rclc_subscription_init_default(&testLED_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Empty), "test_led"));
 
   RCSOFTCHECK(rclc_executor_add_subscription(&executor, &dt_left_sub, &msg, &dt_left_callback, ON_NEW_DATA)); //insane person code
   RCSOFTCHECK(rclc_executor_add_subscription(&executor, &dt_right_sub, &msg, &dt_right_callback, ON_NEW_DATA));
+  RCSOFTCHECK(rclc_executor_add_subscription(&executor, &testLED_sub, NULL, &test_LED_go, ON_NEW_DATA));
   
 
   //ADD NON MICROROS TEST SETUP CODE HERE
@@ -119,10 +127,11 @@ void setup() {
 
 void loop() {
   delay(100);
+  
   #ifdef USING_MICROROS
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
   #else
-  // ???
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
   #endif
 }
 
